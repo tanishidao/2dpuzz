b2d01a779f8f687bb030f49bc84b3da1c0e728aa
tanishidao <tanitaniumauma1003@gmail.com>
2021-03-06 23:21:48 +0900

    リポビタン D

---
+++ Assets/Script/DropCnt.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCnt : MonoBehaviour
+{
+    [SerializeField] Sprite[] sp;
+    /// <summary>
+    /// dropIDの設定
+    /// </summary>
+    ///下は消すときに役立つらしい、要検証
+    public int ID1
+    {
+        get;
+        set;
+    }
+    public int ID2
+    {
+        get;
+        set;
+    }
+    bool touchFlag;
+    Vector2 m;
+    RectTransform r,r2;
+
+    void Start()
+    {
+        ///rにRectTransformを入れる
+        r = GetComponent<RectTransform>();
+        r2 = transform.parent.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (touchFlag)
+        { var pos = Vector2.zero;///zero desu yo
+            m = Input.mousePosition;///mouseの位置取る
+            RectTransformUtility.ScreenPointToLocalPointInRectangle
+                (r2, m, Camera.main, out pos);///
+            r.localPosition = pos;
+        }
+    }
+    /// <summary>
+    /// drop画像に変更
+    /// </summary>
+    /// <param name="n"></param>
+    public void Set(int n)
+    {
+        GetComponent<SpriteRenderer>().sprite = sp[n];
+    }
+public void set(int n)///dropにさわってますぜ関数
+    {
+        GetComponent<SpriteRenderer>().sprite = sp[n];
+
+    }
+public void GetDrop()///drop浮いてる！
+    {
+        touchFlag = true;
+    }public void SetDrop()///dropおいた！
+    {
+        touchFlag = false;
+    }
+
+}
---
+++ Assets/Script/DropGene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropGene : MonoBehaviour
+{
+
+    public GameObject D, L;
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject l = Instantiate(L) as GameObject;
+            l.transform.SetParent(transform);
+            l.transform.localScale = Vector3.one;
+            for (int j = 0; j < 6; j++)
+            {
+                GameObject d = Instantiate(D) as GameObject;
+                d.transform.SetParent(l.transform);
+                int type = Random.Range(0, 6);
+                d.GetComponent<DropCnt>().Set(type);
+                d.GetComponent<DropCnt>().ID1 = i;
+                d.GetComponent<DropCnt>().ID2 = j;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
